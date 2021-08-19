ff628ac7a800a46d9b3353181b7712208cbf5ce4
Li-Cheng Chen <li-cheng.chen@student.adelaide.edu.au>
2021-08-19 19:26:35 +0930

    product item

---
+++ online course/react_shopping_cart/src/component/Products.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Products extends Component{
+    
+    render(){
+        return(
+            <div>
+                <ul className="products">
+                    {this.props.products.map(p => (
+                        <li key={p._id}>
+                            <div className="product">
+                                <a href={"#"+p._id}>
+                                    <img src={p.image} alt={p.title}></img>
+                                    <p>{p.title}</p>
+                                </a>
+                                <div className="price">
+                                    <div>
+                                        {Currency(p.price)}
+                                    </div>
+                                    <button className="btn primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function Currency(num){
+    return "$"+Number(num.toFixed(1)).toLocaleString()+" "; //fixed-point notation
+}
+
+export default Products;
